f0d846b51d5b939d4d3bcaf10014dee40c1c2c1c
liuzilong <liuzilong@jd.com>
2020-04-09 18:14:37 +0800

    01branch

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,30 @@
 // pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
+  data:{
+    name: "liuzilong",
+    age:34,
+    teamer:[
+      {name:'gaoyong',age:35},
+      {name:'gongshanshan',age:30},
+      {name:'wangduanzhen',age:31}
+    ],
+    count:0
+  },
+  zengjia(){
+    this.setData({
+      count:this.data.count+1
+    }
+        
+    )
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+  jianshao(){
+    if (this.data.count>0) {
+      this.setData({
+        count:this.data.count-1
+      })
+    }
+    
 
   }
 })
